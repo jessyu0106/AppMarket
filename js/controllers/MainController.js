dcb6d77c3f6bfdef2566878e2fd6e6f68f607fde
JESS YU <jessyu0106@gmail.com>
2016-07-31 17:49:47 -0400

    init files

---
+++ js/controllers/MainController.js
@@ -0,0 +1,28 @@
+app.controller('MainController', ['$scope', function($scope) {
+  $scope.apps = [ 
+	  { 
+	    icon: 'https://codecademy7893402-8000.ccterminalcloud.com/img/move.jpg', 
+	    title: 'MOVE', 
+	    developer: 'MOVE, Inc.', 
+	    price: 0.99 
+	  }, 
+	  { 
+	    icon: 'https://codecademy7893402-8000.ccterminalcloud.com/img/shutterbugg.jpg', 
+	    title: 'Shutterbugg', 
+	    developer: 'Chico Dusty', 
+	    price: 2.99 
+	  },
+	  {
+	    icon: 'https://codecademy7893402-8000.ccterminalcloud.com/img/gameboard.jpg',
+	    title: 'Gameboard',
+	    developer: 'Armando P.',
+	    price: 1.99
+	  },
+	  {
+	    icon: 'https://codecademy7893402-8000.ccterminalcloud.com/img/forecast.jpg',
+	    title: 'Forecast',
+	    developer: 'Forecast',
+	    price: 1.99
+	  }
+	];
+}]);
